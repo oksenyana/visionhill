21404446507924c16c5a2806f85c63ae5dc6f44a
ntwari <ntwari@700780XPS12.ad.okstate.edu>
2015-09-20 11:25:27 -0500

    Initial logic to get email details: sender, emal, and message.

--- src/main/java/org/visionhill/impl/web/ContactServlet.java
+++ src/main/java/org/visionhill/impl/web/ContactServlet.java
@@ -30,6 +30,23 @@ public class ContactServlet extends VisionHillServlet {
 	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//TODO: missing implementation
+            String sender = req.getParameter("sender");
+            String senderEmail= req.getParameter("senderEmail");
+            String message = req.getParameter("message");
+		
+            //Validate Post Data
+            if( sender != null && sender.length() > 0 &&
+                senderEmail != null && senderEmail.length() > 0 &&
+		 message != null && message.length() > 0)
+		{
+                    //Send email
+                    //Add a config email list.??? libs to use?
+                    resp.getWriter().print("<message>" + sender + ", thank for contact us. Someone will get back to you soon!</message>");
+		}
+		else
+		{
+                    //Send Error Message
+                    resp.getWriter().print("<message>An Error occurred</message>");
+		}
 	}
 }
